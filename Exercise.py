7957eaf06bf24140aa2da7446067947bdfa44e50
a pomaska <a.pomaska.ap@gmail.com>
2022-11-25 22:30:39 +0100

    to do

--- Exercise.py
+++ Exercise.py
@@ -1,23 +1,18 @@
 
 import CheckIt
 
-def sumap(lista):
-    sum = 0
-    for x in lista:
-        if x == 0:
-            break
-        if (x % 2) == 0:
-            sum += x
-    return sum
+def sumanp(lista):
+    # zdefiniuj funkcję i sprawdź jej działanie
+    pass
 
 print("")
 print("Wyniki testów:")
 print("----------------------------------+------------+-----------+-----------")
 print(" Test                             | Oczekiwano | Otrzymano | Wynik")
 print("----------------------------------+------------+-----------+-----------")
-print(" sumap([2, 4, 11, 6, 5, 0, 7, 6]) | 12         | ", sumap([2, 4, 11, 6, 5, 0, 7, 6]),"      |", CheckIt.checkIt(12, sumap([2, 4, 11, 6, 5, 0, 7, 6])))
+print(" sumanp([2, 4, 11, 6, 5, 0, 7, 6])| 16         | ", sumanp([2, 4, 11, 6, 5, 0, 7, 6]),"      |", CheckIt.checkIt(16, sumanp([2, 4, 11, 6, 5, 0, 7, 6])))
 print("----------------------------------+------------+-----------+-----------")
-print(" sumap([1, 1, 0, 1, 1, 6])        | 0          | ", sumap([1, 1, 0, 1, 1, 6]),"       |", CheckIt.checkIt(0, sumap([1, 1, 0, 1, 1, 6])))
+print(" sumanp([1, 1, 0, 1, 1, 6])       | 2          | ", sumanp([1, 1, 0, 1, 1, 6]),"       |", CheckIt.checkIt(2, sumanp([1, 1, 0, 1, 1, 6])))
 print("----------------------------------+------------+-----------+-----------")
-print(" sumap([0])                       | 0          | ", sumap([0]),"       |", CheckIt.checkIt(0, sumap([0])))
+print(" sumanp([0])                      | 0          | ", sumanp([0]),"       |", CheckIt.checkIt(0, sumanp([0])))
 print("")
