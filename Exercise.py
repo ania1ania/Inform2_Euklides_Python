1f2071b8e15039f848da300c0f163e09ac42f13c
a pomaska <a.pomaska.ap@gmail.com>
2022-11-25 21:08:32 +0100

    to do

--- Exercise.py
+++ Exercise.py
@@ -1,27 +1,16 @@
 
 import CheckIt
 
-def nwd(a, b):
-    while b != 0:
-        pom = b
-        b = a % b
-        a = pom
-    return a
-
-def nww(a, b):
-    return (a * b) // nwd(a, b)
-
-def flamastry(x, y, z):
-    pom1 = nww(x, y)
-    pom2 = nww(pom1, z)
-    return pom2 // z
+def suma_cyfr(liczba):
+    # zdefiniuj funkcję i sprawdź jej działanie
+    pass
 
 print("")
 print("Wyniki testów:")
 print(" Test                  | Oczekiwano | Otrzymano | Wynik")
 print("-----------------------+------------+-----------+-----------")
-print(" flamastry(5, 110, 15) | 22         | ", flamastry(5, 110, 15),"      |", CheckIt.checkIt(22, flamastry(5, 110, 15)))
+print(" suma_cyfr(12)         | 3          | ", suma_cyfr(12),"       |", CheckIt.checkIt(3, suma_cyfr(12)))
 print("-----------------------+------------+-----------+-----------")
-print(" flamastry(5, 10, 15)  | 2          | ", flamastry(5, 10, 15),"       |", CheckIt.checkIt(2, flamastry(5, 10, 15)))
+print(" suma_cyfr(179233334)  | 35         | ", suma_cyfr(179233334),"      |", CheckIt.checkIt(35, suma_cyfr(179233334)))
 print("-----------------------+------------+-----------+-----------")
-print(" flamastry(7, 9, 7)    | 9          | ", flamastry(7, 9, 7),"       |", CheckIt.checkIt(9, flamastry(7, 9, 7)))
+print(" suma_cyfr(1)          | 1          | ", suma_cyfr(1),"       |", CheckIt.checkIt(1, suma_cyfr(1)))
