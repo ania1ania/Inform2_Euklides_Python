841e642fce00cd56d5d7a72a1599e775db196bcd
a pomaska <a.pomaska.ap@gmail.com>
2022-11-25 19:46:37 +0100

    working version

--- Exercise.py
+++ Exercise.py
@@ -1,7 +1,12 @@
 
 def ile_cyfr(liczba):
-    # zdefiniuj funkcję i sprawdź jej działanie
-    pass
+    l_cyfr = 0
+    while liczba > 0:
+        liczba //= 10
+        l_cyfr += 1
+    return l_cyfr
 
-print("Powinno być 3 jest ", ile_cyfr(123))
-print("Powinno być 5 jest ", ile_cyfr(17923))
+print("Powinno być 3 jest", ile_cyfr(123))
+print("Powinno być 5 jest", ile_cyfr(17923))
+print("Powinno być 9 jest", ile_cyfr(179233334))
+print("Powinno być 1 jest", ile_cyfr(1))
