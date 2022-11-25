fad3d0ab27c3c2a7183e404288187ec7370e26bb
a pomaska <a.pomaska.ap@gmail.com>
2022-11-25 22:21:31 +0100

    to do

--- Exercise.py
+++ Exercise.py
@@ -1,21 +1,18 @@
 
 import CheckIt
 
-def suma(lista):
-    sum = 0
-    for x in lista:
-        if x == 0:
-            break
-        sum += x
-    return sum
+def sumap(lista):
+    # zdefiniuj funkcję i sprawdź jej działanie
+    pass
 
 print("")
 print("Wyniki testów:")
-print(" Test                                 | Oczekiwano | Otrzymano | Wynik")
-print("--------------------------------------+------------+-----------+-----------")
-print(" suma([2, 4, 11, 6, 5, 0, 7, 6])      | 28         | ", suma([2, 4, 11, 6, 5, 0, 7, 6]),"      |", CheckIt.checkIt(28, suma([2, 4, 11, 6, 5, 0, 7, 6])))
-print("--------------------------------------+------------+-----------+-----------")
-print(" suma([1, 1, 0, 1, 1, 6])             | 2          | ", suma([1, 1, 0, 1, 1, 6]),"       |", CheckIt.checkIt(2, suma([1, 1, 0, 1, 1, 6])))
-print("--------------------------------------+------------+-----------+-----------")
-print(" suma([0])                            | 0          | ", suma([0]),"       |", CheckIt.checkIt(0, suma([0])))
+print("----------------------------------+------------+-----------+-----------")
+print(" Test                             | Oczekiwano | Otrzymano | Wynik")
+print("----------------------------------+------------+-----------+-----------")
+print(" sumap([2, 4, 11, 6, 5, 0, 7, 6]) | 12         | ", sumap([2, 4, 11, 6, 5, 0, 7, 6]),"      |", CheckIt.checkIt(12, sumap([2, 4, 11, 6, 5, 0, 7, 6])))
+print("----------------------------------+------------+-----------+-----------")
+print(" sumap([1, 1, 0, 1, 1, 6])        | 0          | ", sumap([1, 1, 0, 1, 1, 6]),"       |", CheckIt.checkIt(0, sumap([1, 1, 0, 1, 1, 6])))
+print("----------------------------------+------------+-----------+-----------")
+print(" sumap([0])                       | 0          | ", sumap([0]),"       |", CheckIt.checkIt(0, sumap([0])))
 print("")
