e5c03f2a6fa9e7b1af7fc63cdd907975726803b2
a pomaska <a.pomaska.ap@gmail.com>
2022-11-25 22:31:30 +0100

    working

--- Exercise.py
+++ Exercise.py
@@ -2,8 +2,13 @@
 import CheckIt
 
 def sumanp(lista):
-    # zdefiniuj funkcję i sprawdź jej działanie
-    pass
+    sum = 0
+    for x in lista:
+        if x == 0:
+            break
+        if (x % 2):
+            sum += x
+    return sum
 
 print("")
 print("Wyniki testów:")
