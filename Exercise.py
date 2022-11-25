45140d50d89fb85f81941a520cba90d2507e0167
a pomaska <a.pomaska.ap@gmail.com>
2022-11-25 20:45:03 +0100

    working version

--- Exercise.py
+++ Exercise.py
@@ -1,4 +1,10 @@
 
+def checkIt(a, b):
+    if a == b:
+        return "Pozytywny"
+    else:
+        return "Negatywny"
+
 def nwd(a, b):
     while b != 0:
         pom = b
@@ -6,17 +12,19 @@ def nwd(a, b):
         a = pom
     return a
 
+def nww(a, b):
+    return (a * b) // nwd(a, b)
 
 def kiedy(x, y):
-    # zdefiniuj funkcję i sprawdź jej działanie
-    pass
+    return nww(x, y)
 
 print("")
 print("Wyniki testów:")
 print(" Test           | Oczekiwano | Otrzymano | Wynik")
 print("----------------+------------+-----------+-----------")
-print(" kiedy(2, 5)    | 10         | ", kiedy(2, 5),"       |")
+print(" kiedy(2, 5)    | 10         | ", kiedy(2, 5),"      |", checkIt(10, kiedy(2, 5)))
 print("----------------+------------+-----------+-----------")
-print(" kiedy(3, 3)    | 3          | ", kiedy(3, 3),"        |")
+print(" kiedy(3, 3)    | 3          | ", kiedy(3, 3),"       |", checkIt(3, kiedy(3, 3)))
 print("----------------+------------+-----------+-----------")
-print(" kiedy(4, 7)    | 28         | ", kiedy(4, 7),"       |")
+print(" kiedy(4, 7)    | 28         | ", kiedy(4, 7),"      |", checkIt(28, kiedy(4, 7)))
+
