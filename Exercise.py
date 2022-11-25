71c323957160aa4d68be0050bbf739d6ec542877
a pomaska <a.pomaska.ap@gmail.com>
2022-11-25 19:51:42 +0100

    to do

--- Exercise.py
+++ Exercise.py
@@ -1,12 +1,8 @@
 
-def ile_cyfr(liczba):
-    l_cyfr = 0
-    while liczba > 0:
-        liczba //= 10
-        l_cyfr += 1
-    return l_cyfr
+def nwd(a, b):
+	#zdefiniuj funkcję
+    pass
 
-print("Powinno być 3 jest", ile_cyfr(123))
-print("Powinno być 5 jest", ile_cyfr(17923))
-print("Powinno być 9 jest", ile_cyfr(179233334))
-print("Powinno być 1 jest", ile_cyfr(1))
+def rzad(goscie, gospodarze):
+	#zdefiniuj funkcję i sprawdź jej działanie
+	pass
