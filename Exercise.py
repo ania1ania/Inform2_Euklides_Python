cd84b472ac3c9ade61fc9f7fa9db4c74f3f2eb3d
a pomaska <a.pomaska.ap@gmail.com>
2022-11-25 22:12:16 +0100

    working version

--- Exercise.py
+++ Exercise.py
@@ -2,15 +2,20 @@
 import CheckIt
 
 def suma(lista):
-    # zdefiniuj funkcję i sprawdź jej działanie
-    pass
+    sum = 0
+    for x in lista:
+        if x == 0:
+            break
+        sum += x
+    return sum
 
 print("")
 print("Wyniki testów:")
 print(" Test                                 | Oczekiwano | Otrzymano | Wynik")
 print("--------------------------------------+------------+-----------+-----------")
-print(" suma([2, 4, 11, 6, 5, 0, 7, 6])      | 28         | ", suma([6]),"      |", CheckIt.checkIt(28, suma([6])))
+print(" suma([2, 4, 11, 6, 5, 0, 7, 6])      | 28         | ", suma([2, 4, 11, 6, 5, 0, 7, 6]),"      |", CheckIt.checkIt(28, suma([2, 4, 11, 6, 5, 0, 7, 6])))
 print("--------------------------------------+------------+-----------+-----------")
 print(" suma([1, 1, 0, 1, 1, 6])             | 2          | ", suma([1, 1, 0, 1, 1, 6]),"       |", CheckIt.checkIt(2, suma([1, 1, 0, 1, 1, 6])))
 print("--------------------------------------+------------+-----------+-----------")
 print(" suma([0])                            | 0          | ", suma([0]),"       |", CheckIt.checkIt(0, suma([0])))
+print("")
